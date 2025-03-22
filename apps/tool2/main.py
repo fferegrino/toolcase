e03c56a4d4a05a0c07e63bcb9dacd740b763a425
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-03-22 22:04:08 +0000

    Add ruff to an app

--- apps/tool2/main.py
+++ apps/tool2/main.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import typer
 from toolcase import __version__
+
 from utils import hola
 
 app = typer.Typer()
+
 
 @app.command()
 def main(name: str | None = None):
     if not name:
         name = "World"
-
     print(f"Hello, {name}! (toolcase version: {__version__})")
+    print(f"Pandas version: {pd.__version__}")
     hola(name)
+
+
 if __name__ == "__main__":
     app()
