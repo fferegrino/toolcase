7a0a2e5be654dfe85541f8fd95238ae3d72dad4e
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-03-24 16:30:12 +0000

    Run formatting

--- apps/tool2/main.py
+++ apps/tool2/main.py
@@ -7,12 +7,7 @@ app = typer.Typer()
 
 @app.command()
 def main(columns: int = 10, rows: int = 10) -> None:
-    df = pd.DataFrame(
-        {
-            chr(i): range(rows)
-            for i in range(columns)
-        }
-    )
+    df = pd.DataFrame({chr(i): range(rows) for i in range(columns)})
     print(uppercase_columns(df))
 
 
